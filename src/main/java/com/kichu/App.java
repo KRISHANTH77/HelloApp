a99786673a9a4a3945a250b1e4e80e5fdb5634c6
KRISHANTH77 <krishanthvamsi07@gmail.com>
2026-03-19 15:45:18 +0530

    Added UC1 program

--- src/main/java/com/kichu/App.java
+++ src/main/java/com/kichu/App.java
@@ -2,7 +2,6 @@ package com.kichu;
 
 public class App {
     public static void main(String[] args) {
-        String name = args[0];
-        System.out.println("Hello " + name);
+        System.out.println("Hello World!");
     }
 }
